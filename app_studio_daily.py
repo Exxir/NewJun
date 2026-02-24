065f8cd4cbe8a834ab5f7b40355e76e8a8e276b5
Michael Nazerian <mnaz@Michaels-Air.search.charter.net>
2026-02-23 18:51:11 -0600

    Move Sales tab second and rename

--- app_studio_daily.py
+++ app_studio_daily.py
@@ -448,7 +448,7 @@ st.markdown(
 )
 
 # --- Layout ---
-tab_snap, tab_sales, tab_chart, tab_visits, tab_forecast, tab_occupancy, tab_fw_dashboard, tab_sales_money = st.tabs(["Snap", "Current", "Chart", "Visits", "Forecast", "Occupancy", "Summary", "Sales $"])
+tab_snap, tab_sales_money, tab_sales, tab_chart, tab_visits, tab_forecast, tab_occupancy, tab_fw_dashboard = st.tabs(["Snap", "Sales", "Current", "Chart", "Visits", "Forecast", "Occupancy", "Summary"])
 
 with tab_sales:
     col1, col2 = st.columns([1, 1])
