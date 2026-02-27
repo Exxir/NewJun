1d63b0d5acb29c93457da0b2fdd799afa8a91eba
Michael Nazerian <mnaz@Michaels-MacBook-Air.local>
2026-02-27 08:05:38 -0600

    Rename Trips tab to Visits

--- app_studio_daily.py
+++ app_studio_daily.py
@@ -659,7 +659,7 @@ st.markdown(
 )
 
 # --- Layout ---
-tab_snap, tab_new_snap, tab_sales_money, tab_trips, tab_sales, tab_chart, tab_visits, tab_forecast, tab_occupancy, tab_fw_dashboard = st.tabs(["Snap", "New Snap", "Sales", "Trips", "Current", "Chart", "Clients", "Forecast", "Occupancy", "Summary"])
+tab_snap, tab_new_snap, tab_sales_money, tab_trips, tab_sales, tab_chart, tab_visits, tab_forecast, tab_occupancy, tab_fw_dashboard = st.tabs(["Snap", "New Snap", "Sales", "Visits", "Current", "Chart", "Clients", "Forecast", "Occupancy", "Summary"])
 
 with tab_sales:
     col1, col2 = st.columns([1, 1])
